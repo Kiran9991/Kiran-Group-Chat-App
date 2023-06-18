d1a0d8000b82e4241f2bbf094c3c6230f87cca54
Suraj <122242670+Kiran9991@users.noreply.github.com>
2023-06-18 19:23:53 +0530

    Created the frontend page of chat app

--- controller/userController.js
+++ controller/userController.js
@@ -57,16 +57,16 @@ const login = async (req, res) => {
                     throw new Error('Something went wrong');
                 }
                 if(result === true) {
-                    res.status(200).json({ success: false, message: 'User logged in successfully', token: generateAccessToken(user[0].id, user[0].name) })
+                    res.status(200).json({ message: 'User logged in successfully', token: generateAccessToken(user[0].id, user[0].name) })
                 }else {
-                    return res.status(401).json({success: false, error: 'User not authorized'});
+                    return res.status(401).json({ error: 'User not authorized'});
                 }
             })
         }else {
-            return res.status(404).json({success: false, error: `User not found`})
+            return res.status(404).json({ error: `User not found`})
         }
     } catch(err) {
-        res.status(500).json({error: err, success: false});
+        res.status(500).json({ error: err });
     }
 }
 
