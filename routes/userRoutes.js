1e932173c3358099a416d9b342968f9b7449acac
Suraj <122242670+Kiran9991@users.noreply.github.com>
2023-07-25 17:09:42 +0530

    complete

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const userController = require('../controller/userController');
 
 const router = express.Router();
 
-router.post('/signup-user', userController.signup);
+router.get('/signup-user', userController.signup);
 
 router.post('/login-user', userController.login);
 
